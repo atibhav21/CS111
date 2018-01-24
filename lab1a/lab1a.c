3210d5bba8060b342aa82fc402ecc6158285cfea
atibhav21 <atibhav.mittal6@gmail.com>
2018-01-24 12:19:50 -0800

    Working lab1a version

--- lab1a/lab1a.c
+++ lab1a/lab1a.c
@@ -19,14 +19,14 @@ ID: 804598987
 
 extern int errno;
 
+struct termios old_settings;
+
 #define TRUE 1
 #define FALSE 0
 
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
-
-struct termios old_settings;
 
 void createNewSettings(struct termios* settings, struct termios old_settings)
 {
@@ -36,45 +36,43 @@ void createNewSettings(struct termios* settings, struct termios old_settings)
 	settings->c_lflag = 0;		/* no processing	*/
 }
 
-void signal_handler(int x)
+void exitError(char* message)
 {
-	fprintf(stderr, "Received SIGPIPE: %d\r\n", x);
+	fprintf(stderr, "Error received: %s\r\n", message);
 	tcsetattr(1, TCSANOW, &old_settings);
 	exit(1);
 }
 
 // Process the input and output it
 // if from_terminal argument is set to true, then forward the buffer to the FD referred to by fd_to_shell
-// child_id is the process id of the child process created by the main executable program
-void processInputs(int shell_active, int fd_to_shell, char* buffer, int num_read, int from_terminal, pid_t child_id)
+// 
+int processInputs(int shell_active, int fd_to_shell, char* buffer, int num_read, int from_terminal, int child_id)
 {
 	if(from_terminal)
 	{
 		int i;
 		for(i = 0; i < num_read; i += 1)
 		{
-			if(buffer[i] == 3) // ^C received
+			if(buffer[i] == 3)
 			{
-				// send a kill signal to the shell and return
-				//fprintf(stderr, "Kill Called\r\n");
-				if(kill(child_id, SIGINT) == -1 )
-				{
-					fprintf(stderr, "Error while sending kill signal: %s\r\n", strerror(errno));
-					tcsetattr(1, TCSANOW, &old_settings);
-					exit(1);
+				fprintf(stderr, "^C pressed, Killing child_id: %d\r\n", child_id);
+				if(kill((pid_t) child_id, SIGINT) == -1)
+				{	
+					exitError("Error while sending SIGINT to shell");
 				}
-				//return 1;
+				else
+				{
+					fprintf(stderr, "At least one signal was sent\r\n");	
+				}
+				write(1, &buffer[i], 1);
+				return 0; // continue with the processing
 			}
 			if(buffer[i] == 4) // ^D received
 			{
 				// end of file received
-				if( close(fd_to_shell) == -1)
-				{
-					fprintf(stderr, "Error while closing shell descriptor: %s\r\n", strerror(errno));
-					tcsetattr(1, TCSANOW, &old_settings);
-					exit(1);
-				}
-				//return 1;
+				write(1, "^D", 2);
+				close(fd_to_shell);
+				return 1;
 			}
 			else if(i+1 < num_read && buffer[i] == '\r' && buffer[i] == '\n')
 			{
@@ -92,9 +90,8 @@ void processInputs(int shell_active, int fd_to_shell, char* buffer, int num_read
 				{
 					if(write(fd_to_shell, "\n", 1) == -1)
 					{
-						fprintf(stderr, "Error: %s\n", strerror(errno));
-						tcsetattr(1, TCSANOW, &old_settings);
-						exit(1);
+						//fprintf(stderr, "Error: %s\n", strerror(errno));
+						exitError(strerror(errno));
 					}
 				}
 			}
@@ -105,9 +102,7 @@ void processInputs(int shell_active, int fd_to_shell, char* buffer, int num_read
 				{
 					if(write(fd_to_shell, &buffer[i], 1) == -1)
 					{
-						fprintf(stderr, "Error: %s\n", strerror(errno));
-						tcsetattr(1, TCSANOW, &old_settings);
-						exit(1);
+						exitError(strerror(errno));
 					}
 				}
 			}
@@ -118,26 +113,28 @@ void processInputs(int shell_active, int fd_to_shell, char* buffer, int num_read
 		int i;
 		for(i = 0; i < num_read; i += 1)
 		{
-			if(buffer[i] == 4)
+			/*if(buffer[i] == 4)
 			{
-				// EOF file received
-				//write(1, "End of File received\n", 20);
-				//write(1, "\r\n", 2);
-				fprintf(stderr, "End of File received\r\n");
-			}
+				write(1, "\r\n", 2);
+			}*/ 
 			if(buffer[i] == '\r' || buffer[i] == '\n')
 			{
-				write(1, "\r\n", 2); //TODO: Check if this needs to be changed to spaces or any other thing 
+				if(write(1, "\r\n", 2) == -1) //TODO: Check if this needs to be changed to spaces or any other thing 
+				{
+					exitError(strerror(errno));
+				}
 			}
 			else
 			{
-				write(1, &buffer[i], 1);
+				if(write(1, &buffer[i], 1) == -1)
+				{
+					exitError(strerror(errno));
+				}
 			}
 		}
 		//write(1, buffer, num_read);
 	}
-
-	//return 0;
+	return 0;
 }
 
 // 
@@ -145,6 +142,7 @@ void performIO(int shell_active, int fd_to_shell, int fd_from_shell, int child_i
 {
 	char buff[10];
 	char buff_shell[256];
+	int eof_received = 0;
 
 	struct pollfd two_inputs[2];
 
@@ -157,117 +155,93 @@ void performIO(int shell_active, int fd_to_shell, int fd_from_shell, int child_i
 		two_inputs[1].events = POLLIN | POLLHUP | POLLERR;
 	}
 
+	signal(SIGPIPE, SIG_IGN);
 
-	while(1) 
+	int poll_result;
+	while(! eof_received)// && ! sigpipe_received) 
 	{
-		if(poll(two_inputs, 2, 0) > 0)
+		poll_result = poll(two_inputs, 2, 0);
+		if(poll_result > 0)
 		{
-			signal(SIGPIPE, signal_handler);
 			if(two_inputs[0].revents & POLLIN)
 			{
 				// receiving input from the terminal
-				fprintf(stdout, "Hitting Point A\r\n");
 				int num_read = read(0, buff, 10);
-				processInputs(shell_active, fd_to_shell, buff, num_read, TRUE, child_id);
+				if(num_read == -1)
+				{
+					exitError(strerror(errno));
+				}
+				eof_received = processInputs(shell_active, fd_to_shell, buff, num_read, TRUE, child_id);
 			}
-			else if(two_inputs[0].revents & POLLHUP)
+			else if((two_inputs[0].revents & (POLLHUP | POLLERR)))
 			{
 				// received some kind of error from the terminal
-				// TODO!!
-				fprintf(stdout, "Hitting Point B\r\n");
-				fprintf(stderr, "Something received in first event\r\n");
-				break;
-			}
-			else if(two_inputs[0].revents & POLLERR)
-			{
-				fprintf(stderr, "Something error occured during polling: %s\r\n", strerror(errno));
-				tcsetattr(1, TCSANOW, &old_settings);
-				exit(1);
+				exitError(strerror(errno));
 			}
 			else if(shell_active && two_inputs[1].revents & POLLIN)
 			{
-				
 				// receiving input from the shell descriptor
-				fprintf(stdout, "Hitting Point C\r\n");
 				int num_read = read(fd_from_shell, buff_shell, 256);
-				processInputs(shell_active, fd_to_shell, buff_shell, num_read, FALSE, child_id);
+				if(num_read == -1)
+				{
+					exitError(strerror(errno));
+				}
+				eof_received = processInputs(shell_active, fd_to_shell, buff_shell, num_read, FALSE, child_id);
 			}
-			else if(shell_active && (two_inputs[1].revents & POLLHUP))
+			else if(shell_active && (two_inputs[1].revents & POLLHUP ))
 			{
-				//fprintf(stderr, "Does this get reached?\r\n");
-
-				
-				// received eof from the shell
-				// close write pipes from shell back to main lab1a executable
-				//close()
-				fprintf(stdout, "Hitting Point D\r\n");
-				int num_read;
-				// keep doing a read  until it returns 0, since hangup has occured, after receiving 0, 
-				// use wait pid to get shell's exit status and report it to stderr
-				// TODO: Uncomment
-				while((num_read = read(fd_from_shell, buff_shell, 256)) != 0)
-				{
-					if(num_read == -1)
-					{
-						fprintf(stderr, "Some error occured during read/write from shell: %s\n", strerror(errno));
-						tcsetattr(1, TCSANOW, &old_settings);
-						exit(1);
-					}
-					else
-					{
-						processInputs(shell_active, fd_to_shell, buff_shell, num_read, FALSE, child_id);
-					}
-				}
-
-				int shell_status;
-				if(waitpid(child_id, &shell_status, 0) == -1)
-				{
-					// error
-					fprintf(stderr, "Error occured while exiting shell\n");
-					tcsetattr(1, TCSANOW, &old_settings);
-					exit(1);
-				}
-				else
-				{
-					// successfully finished reading all of the shell's output
-					fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\r\n", (shell_status & 0x007f), (shell_status & 0xff00));
-				}
-				
-				//fprintf(stderr, "Something received in second event\r\n" );
+				// all file descriptors to the shell have been closed so exit after processing all read input fromt the shell
+				fprintf(stderr, "POLLHUP received\r\n");
 				break;
 			}
 			else if(shell_active && (two_inputs[1].revents & POLLERR))
 			{
-				fprintf(stdout, "Hitting Point E\r\n");
-				int shell_status;
-				if(waitpid(child_id, &shell_status, 0) == -1)
-				{
-					// error
-					fprintf(stderr, "Error occured while exiting shell\r\n");
-					tcsetattr(1, TCSANOW, &old_settings);
-					exit(1);
-				}
-				else
-				{
-					// successfully finished reading all of the shell's output
-					fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\r\n", (shell_status & 0x007f), (shell_status & 0xff00));
-				}
-				break;
+				// received error from the shell
+				exitError(strerror(errno));
 			}
+		}
+		else if(poll_result == 0)
+		{
+			continue;
+		}
+		else if(poll_result == -1)
+		{
+			exitError(strerror(errno));
+		}
+		//write(1, buff, num_read);
+	}
+	int num_read;
+	while(1)
+	{
+		// read all remaining output from the shell
+		num_read = read(fd_from_shell, buff_shell, 256);
+		if(num_read == 0)
+		{
+			break;
+		}
+		else if(num_read == -1)
+		{
+			exitError(strerror(errno));
 		}
 		else
 		{
-			fprintf(stdout, "Hitting Point F\r\n");
-			continue;
+			if(write(1, buff_shell, num_read) == -1)
+			{
+				exitError(strerror(errno));
+			}
 		}
 	}
-}
-
-void exitError(char* message)
-{
-	fprintf(stderr, "%s: %s\r\n", message, strerror(errno) );
-	tcsetattr(1, TCSANOW, &old_settings);
-	exit(1);
+	int shell_status;
+	if(waitpid(child_id, &shell_status, 0) == -1)
+	{
+		// error
+		exitError("Error occured while exiting shell");
+	}
+	else
+	{
+		// successfully finished reading all of the shell's output
+		fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\r\n", (shell_status & 0x007f), ((shell_status & 0xff00) >> 8));
+	}
 }
 
 int main(int argc, char *argv[])
@@ -281,12 +255,30 @@ int main(int argc, char *argv[])
 	int parent_to_child[2];
 	int child_to_parent[2];
 
+
 	static struct option long_options[] = 
 				{
 					{"shell", no_argument, 0, 's'},
 					//{"debug", no_argument, 0, 'd'},
 					{0, 0, 0, 0}
 				};
+	int c = getopt_long(argc, argv, "", long_options, &option_index);
+	while(c != -1)
+	{
+		switch(c)
+		{
+			case 's': in_shell_mode = TRUE;
+					  break;
+			/*case 'd': debug_enabled = TRUE;
+					  break;*/
+			default:
+				fprintf(stderr, "usage: ./lab1 [--shell]\n");
+				exit(1);
+		}
+
+		c = getopt_long(argc, argv, "", long_options, &option_index);	
+	}
+
 
 	if(tcgetattr(1 , &old_settings) != 0)
 	{
@@ -303,28 +295,9 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	int c = getopt_long(argc, argv, "", long_options, &option_index);
-
-	while(c != -1)
-	{
-		switch(c)
-		{
-			case 's': in_shell_mode = TRUE;
-					  break;
-			/*case 'd': debug_enabled = TRUE;
-					  break;*/
-			default:
-				fprintf(stderr, "Usage: ./lab1a [--shell]\r\n");
-				exitError("Incorrect Option Passed, Exiting program now...");
-				break; // Will never reach this point
-		}
-
-		c = getopt_long(argc, argv, "", long_options, &option_index);	
-	}
-
 	/*
-	* parent_to_child is the pipe that takes data from user process and sends it to the shell
-	* child_to_parent is the pipe that sends the output of the shell execution to the parent(lab1a) process
+	* parent_to_child[0] -> parent_to_child[1] is the pipe that takes data from user process and sends it to the shell
+	* child_to_parent[0] -> child_to_parent[1] is the process that sends the output of the shell execution to the parent(lab1a.c) process
 	*/
 	if(in_shell_mode)
 	{
@@ -336,17 +309,14 @@ int main(int argc, char *argv[])
 		{
 			exitError("Could not create pipe");
 		}
-
-		int child_id = fork();
-		if(child_id < 0)
+		int rc = fork();
+		if(rc < 0)
 		{
-			exitError("Fork failed");
+			exitError("Fork Failed");
 		}
-		else if(child_id == 0)
+		else if(rc == 0)
 		{
-
 			// Child process, do file redirection, and then call exec to run the shell
-
 			if(close(child_to_parent[0]) != 0 ||  // close read end of the pipe from child to parent in the child process
 						close(parent_to_child[1])) { // close write end of the pipe from parent to child in the child process
 				exitError("Error in Closing the pipe");
@@ -378,20 +348,21 @@ int main(int argc, char *argv[])
 		else
 		{
 			// parent process
-			// close read end of parent_to_child descriptor
-			// close write end of child_to_parent descriptor
 			if(close(parent_to_child[0]) != 0 || close(child_to_parent[1]) != 0)
 			{
 				exitError("Error occured while closing pipe ends");
 			}
-			performIO(TRUE, parent_to_child[1], child_to_parent[0], child_id);
+			performIO(TRUE, parent_to_child[1], child_to_parent[0], rc);
 		}
 	}
+	
 	else
 	{
 		// work in regular mode (non-shell)
 		performIO(FALSE, parent_to_child[1], child_to_parent[0], -1) ;
 	}
+
+
 	tcsetattr(1, TCSANOW, &old_settings); // reset to old settings
 	return 0;
 }
